bf3a9175f2ce8bfb194e6c3ae1b6ab3bf7c8c8c9
Gustavo Rodrigues <gustavohrx123@gmail.com>
2023-10-01 20:10:16 -0300

    Mais um exercício

--- exercicios_beecrowd/desafio_do_maior_numero_2414/main.c
+++ exercicios_beecrowd/desafio_do_maior_numero_2414/main.c
@@ -1,3 +1,12 @@
+//ID_BEE      RA        NOME
+//798447      114574    Gustavo Henrique Rodrigues
+//811892      115826    Leonardo Vinícius Pais de Oliveira
+//799251      114975    Gustavo Henrique Timachi
+//801498      114971    Danilo Comin Honorato da Silva
+//801474      115380    Márcio Roberto Soares Perereira Junior
+//806478      113656    Alefe Thiago da Silva Cirino
+
+
 #include <stdio.h>
 
 int main(){
